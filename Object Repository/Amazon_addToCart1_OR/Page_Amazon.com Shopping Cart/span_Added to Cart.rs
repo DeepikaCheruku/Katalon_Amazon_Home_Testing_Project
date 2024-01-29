a7dbb9193563bb1c7d3051ad6903b000fd5d9e8f
dell <dell@192.168.2.6>
2024-01-29 20:49:10 +0530

    My 1st project commit

---
+++ Object Repository/Amazon_addToCart1_OR/Page_Amazon.com Shopping Cart/span_Added to Cart.rs
@@ -0,0 +1,87 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_Added to Cart</name>
+   <tag></tag>
+   <elementGuidId>51f61f0f-1b34-4e28-a7e0-a6662f8531cf</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>span.a-size-medium-plus.a-color-base.sw-atc-text.a-text-bold</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='NATC_SMART_WAGON_CONF_MSG_SUCCESS']/span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+      <webElementGuid>a2e93596-1b57-4138-a82e-265bd0567115</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>a-size-medium-plus a-color-base sw-atc-text a-text-bold</value>
+      <webElementGuid>e3a09994-9e33-4694-b189-a2bfe8bf63bf</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+        
+            Added to Cart
+        
+    </value>
+      <webElementGuid>11d9df7d-4bb4-407f-83f2-c5451abfc63a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;NATC_SMART_WAGON_CONF_MSG_SUCCESS&quot;)/span[@class=&quot;a-size-medium-plus a-color-base sw-atc-text a-text-bold&quot;]</value>
+      <webElementGuid>5cb4f5a2-b591-4941-8f33-4eface744134</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='NATC_SMART_WAGON_CONF_MSG_SUCCESS']/span</value>
+      <webElementGuid>0159757f-e2e1-4826-9d59-7d394b7c2a48</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/div/div/div/div/div[2]/div/div/div/div/span</value>
+      <webElementGuid>db4004c7-9fb2-4deb-a978-0c16cb42fa4e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//span[(text() = '
+        
+            Added to Cart
+        
+    ' or . = '
+        
+            Added to Cart
+        
+    ')]</value>
+      <webElementGuid>8fb32782-8c37-4fcb-b1ea-12a5687bbdc7</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/practice_tests/amazon.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>amazon</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ae62e918-538b-4b0d-8ab4-e551fee704ba</testSuiteGuid>
+   <testCaseLink>
+      <guid>a2fa2312-aee2-4244-8976-e63e936bcd8a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AmazonProject/TC_Amazon_Home_SV_003</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>e6558004-eb92-4465-b899-e3ecf445eb2f</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/DDT_TestData/Amazon_Search_excel_folder</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>e6558004-eb92-4465-b899-e3ecf445eb2f</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>category</value>
+         <variableId>8da9dc91-9fb4-4f81-9519-8e886bcec16a</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>e6558004-eb92-4465-b899-e3ecf445eb2f</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>item</value>
+         <variableId>99393b23-cf64-487b-8f26-df8f45d08007</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
